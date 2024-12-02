6126b4e4cde39b681e36f863545346e6eeb8019d
robinrolle <robinrolle1748@gmail.com>
2024-12-02 08:36:22 +0100

    [MIG] partner_tag_smart_assignation: Migration to 16.0

--- partner_tag_smart_assignation/__manifest__.py
+++ partner_tag_smart_assignation/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Smart Tagger",
     "summary": "Smart tagger, module to have smart tags who " "update themselves alone",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Other",
     "author": "Compassion CH, Odoo Community Association (OCA)",
     "license": "AGPL-3",
